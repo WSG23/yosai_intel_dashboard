5d50200e681a7efe061340c7f1d823ae75be4f27
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-06-19 17:02:51 +0900

    Fix dash run_server deprecation

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def create_safe_app():
 
 if __name__ == "__main__":
     app = create_safe_app()
-    app.run_server(debug=True, host="0.0.0.0", port=8050)
+    app.run(debug=True, host="0.0.0.0", port=8050)
 
