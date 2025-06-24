097cd5bb63204bb2b64dcf3896f32bc705eb3300
WSG23 <trb427@gmail.com>
2025-06-24 13:05:44 +0900

    Remove Test

--- tests/test_translation_coverage.py
+++
@@ -1,8 +0,0 @@
-from scripts import check_missing_translations
-
-
-def test_translations_complete():
-    try:
-        check_missing_translations.main()
-    except SystemExit:
-        assert False, "Missing translations"
